a25760aba1f7dcca606e7ad32e8c010a0dd0d9f7
bachelier_mathis <06mathis08@gmail.com>
2023-08-06 18:34:07 +0200

    connection page save

--- bdd.php
+++ bdd.php
@@ -1,8 +1,4 @@
 <?php
-// Souvent on identifie cet objet par la variable $conn ou $db
-$mysqlConnection = new PDO(
-    'mysql:host=localhost;dbname=projet_septembre;charset=utf8',
-    'root',
-    'root'
-);
+
+$BDD = new PDO('mysql:host=localhost;dbname=projet_septembre;charset=UTF8','root','');
 ?>
