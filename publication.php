85830d5a272b030ace50a75107e27bc6eb2cf870
bachelier_mathis <06mathis08@gmail.com>
2023-09-10 21:31:33 +0200

    post css

--- publication.php
+++ publication.php
@@ -93,7 +93,7 @@
                 </div>
             </div>
         </nav>
-    <form method="post" enctype="multipart/form-data">
+    <!-- <form method="post" enctype="multipart/form-data">
         <input type="text" name="recette_titre" placeholder='le nom de la recette' <?php if($mode_edition == 1) {
           ?>  value="<?= $edit_article['nom']?>"<?php } ?> required ><br>  
         <select name="categories" <?php if($mode_edition == 1) {
@@ -118,14 +118,76 @@
         <br>
         <input type="submit" value="submit">
         </form>
-        <br>
+        <br> -->
 
         <?php if(isset($message)) {echo $message;} ?>
     </form>
-    
+    <div class="content">
+        <div class="container">
+            <div class="row align-items-stretch no-gutters contact-wrap">
+            <div class="col-md-12" style="padding-top: 20px; padding-bottom: 20px !important; border: thick double #cdb4db; border-radius: 15px; margin-top: 15px !important ; margin-bottom: 15px !important ;">
+            <div class="form h-100">
+            <h3>Poste ta recette :)<!DOCTYPE html></h3>
+            <form class="mb-5" method="post" enctype="multipart/form-data">
+            <div class="row">
+            <div class="col-md-6 form-group mb-3">
+            <label for="" class="col-form-label">Le titre de ta recette :</label><br>
+            <input type="text"  class="form-control" name="recette_titre" placeholder='le nom de la recette' <?php if($mode_edition == 1) {
+          ?>  value="<?= $edit_article['nom']?>"<?php } ?> required >
+            </div>
+            <div class="col-md-12 form-group mb-3" style="align-items: center;">
+            <label for="budget" class="col-form-label" >Choissi la catégorie de ta recette :</label><br>
+            <select name="categories" <?php if($mode_edition == 1) {
+            ?> value="<?= $edit_article['categorie']?>"<?php } ?> required>
+            <option value="">--choisie la catégorie</option>
+            <option value="entré">entré</option>
+            <option value="plat">plat</option>
+            <option value="dessert">dessert</option>
+            <option value="appéritif">appéritif</option>
+            <option value="autre">autre</option>
+            </select>
+            </div>
+            </div>
+            <div class="row">
+            <div class="col-md-12 form-group mb-3">
+            <label for="" class="col-form-label">Description :</label>
+            <textarea class="form-control" name="intro" placeholder="Donne nous une petite description de ta recettes :" required><?php if($mode_edition == 1) {
+          ?><?= $edit_article['intro']?><?php }?></textarea>
+            
+            </div>
+            </div>
+            <div class="row">
+            <div class="col-md-12 form-group mb-3">
+            <label for="message" class="col-form-label">préparation :</label>
+            <textarea class="form-control" name="descriptif_rec" placeholder="Ecrit les ingrédients et les étapes de préparation de ta recette " required><?php if($mode_edition == 1) {
+          ?><?= $edit_article['descriptions']?><?php }?></textarea>
+            </div>
+            <?php if($mode_edition == 0) {
+          ?>
+            <div class="row">
+                <div class="col-md-12 form-group mb-3">
+                <label for="message" class="col-form-label">image à upload :</label> <br>
+                <input type="file" name="miniature" >
+            </div> <?php } ?> 
+        <br>
+
+            </div>
+            <div class="row">
+            <div class="col-md-12 form-group">
+            <input type="submit" value="submit" class="btn btn-primary rounded-0 py-2 px-4" style="border-radius: 15px !important; border: solid;">
+            
+            </div>
+            </div>
+            </form>
+            
+            </div>
+            </div>
+            </div>
+        </div>
+</div>
     
 </body>
-        <footer class="py-5 bg-dark" style="background-color:  !important;">
+        <footer class="py-5 bg-dark" style="background-color: #6a994e !important;">
             <div class="container"><p class="m-0 text-center text-white">Copyright © CooK'nShare 2023</p></div>
         </footer>
 </html>
